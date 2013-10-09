640a9f7a754135d3a23a33338137c02a96335496
Kevin Shrestha <kev_shrestha@hotmail.com>
2013-10-09 19:31:43 +0200

    added comments explaining variables

--- output.h
+++ output.h
@@ -2,41 +2,31 @@
 #define data 	1
 #define clk 	3
 
-int displayControl(int period, int hRGB)
+int displayControl(int queue, int hRGB)
 {
 	int R, G, B;
 	
 	int regBits[24];
 
 
-	if ((R % period) == 0){
-
-	}	
-	if ((G % period) == 0){
-
-	}	
-	if ((B % period) == 0){
-
-	}	
-
 	return regBits;
 }
 
-void shiftOut (int8_t pointer, int8_t period, int regBits[24])
+void shiftOut (int counter, int queue, int regBits[24])
 {
 	//Clock
-	if ((pointer % 2) == 0){
+	if ((counter % 2) == 0){
 		PORTB = (1 << clk);
 	} else {
 		PORTB = (0 << clk);
 	}
 	
 	//Strobe
-	if (pointer == period - 2)	PORTB = (1 << str);
-	if (pointer == period - 1)	PORTB = (0 << str);
+	if (counter == queue - 2)	PORTB = (1 << str);
+	if (counter == queue - 1)	PORTB = (0 << str);
 
-
-	if ((pointer % 2) == 0 && pointer < (period - 2)){
-		PORTB = (regBits[pointer/2] << pointer/2);
+	//Data
+	if ((counter % 2) == 0 && counter < (queue - 2)){
+		PORTB = (regBits[counter/2] << data);
 	}
 }
--- src/ppp.c
+++ src/ppp.c
@@ -11,9 +11,12 @@
 #define data 	1			//DATA PIN
 #define clk 	3			//CLOCK PIN
 
-volatile int8_t pointer = 0;
-volatile int8_t period = 50;
-volatile int8_t regBits[24];
+volatile int counter = 0;	
+volatile int queue = 50;	//Time (1 period) used for transfering data to the shift register (also: amount of clocks to empty the queue)
+volatile int regBits[24];	//Contents of the shift registers in array format [Number of bits that can be stored in the shift registers]
+volatile int freq = 60;		//Frequency (in Hz) of LED color value updates
+volatile int t = 50;		//Time period (in ms) of one LED color value before update - do not use both frequency and period
+
 
 void init (void)
 {
@@ -23,13 +26,13 @@ void init (void)
 	PORTA	= 	0b11111111;
 	PORTB	= 	0b00000100;
 
-	GICR 	= 	1<<INT2;			//aktiviert Interrupt
-	MCUCSR 	|= 	(1<<ISC2);		//steigende Flanke
+	//GICR 	= 	1<<INT2;			//aktiviert Interrupt
+	//MCUCSR 	|= 	(1<<ISC2);		//steigende Flanke
 
 	//Timer 1
-	TCCR1B 	|= 	(0<<CS12)|(1<<CS11)|(1<<CS10);	//clk/64 prescaler - 250000Hz
-	TIMSK 	|= 	(1<<TOIE1);	
-	TCNT1	=	0x00;
+	TCCR1B 	= 	0b00000011;		//(0<<CS12)|(1<<CS11)|(1<<CS10);	//clk/64 prescaler - 250000Hz
+	TIMSK 	= 	(1<<TOIE1);		//Timer/Counter1, Overflow Interrupt Enable
+	TCNT1	=	0;				//Timer/Counter1 reset
 
 	sei();	
 				
@@ -39,30 +42,39 @@ int main()
 {
 	init();
 
-	//lauflicht();
+	//lauflicht();		//execute lauflicht - do activate while timers are in use since _delay is utilized
 	//tasterTest();
 }
 
 
 ISR(TIMER1_OVF_vect)
 {
-	if(pointer < period)
+	if(counter < queue)					
 	{
-		pointer++;
+		counter++;
+	} else {
+		counter = 0;
 	}
-	else
-	{
-		PORTA ^= 0xFF;
-		pointer = 0;
-	}
-	shiftOut(*pointer, period, regBits);
-}
 
+	if ((counter % 100000) == 0){
+		PORTA ^= 0b11111111;
+	}
+/*
+	if (counter % freq == 0) {
+		shiftOut(&counter, queue, &regBits);
+	}
+*/
+	if (counter == t*10000) {
+		shiftOut(&counter, queue, &regBits);
+	}
+
+}
+/*
 ISR(INT2_vect)
 {
 	PORTA	^= 	0b11111111;
 }
-
+*/
 
 
 
